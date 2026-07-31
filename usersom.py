aaa10b95865186a7c06578d64238bfa798704293
Md Salim <salimm0118@gmail.com>
2026-07-31 11:28:32 +0530

    sum 2, square  no

--- usersom.py
+++ usersom.py
@@ -2,3 +2,9 @@ first = int(input("enter first :" ))
 second = int(input("enter second :" ))
 
 print("sum =", first + second)
+
+
+side = float(input("enter square :" ))
+print("area =", side * side)
+
+
