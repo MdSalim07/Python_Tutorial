fe12749f4c061c6e6ad3803cc2d082827b66f79a
Md Salim <salimm0118@gmail.com>
2026-07-31 10:19:50 +0530

    All  operator

--- Python_Tutorial/arithmetic.py
+++ Python_Tutorial/arithmetic.py
@@ -1,4 +1,4 @@
-# arithmetic Operator
+# arithmetic Operator(+,-,*,/,%,**)
 a = 3
 b = 4
 print(a + b)
@@ -6,3 +6,49 @@ print(a - b)
 print(a * b)
 print(a % b)
 print(a ** b)
+
+#  Relational/Comparison Operator (==,!=,>,<,>=,<=)
+
+a = 50
+b = 20
+print(a == b) 
+print(a != b)
+print(a >= b) 
+print(a <= b) 
+print(a < b) 
+print(a > b)
+
+# assigment operator
+
+
+num = 10
+num += 10
+print("num :",num)
+
+
+num = 10
+num *= 10
+print("num :",num)
+
+
+num = 11
+num %= 10
+print("num :",num)
+
+
+
+num = 10
+num **= 2
+print("num :",num)
+
+# logical operator
+a = 40
+b = 20
+print(not False)
+print(not (a > b))
+value1 = True
+value2 = True
+print("and operator:", value1 and value2)
+print("OR operatorL:", (a == b) or (a > b))
+
+
