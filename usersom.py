64667c635d8efb0d20f4fcfef8f5cee0dfc614b7
Md Salim <salimm0118@gmail.com>
2026-07-31 11:36:23 +0530

    added

--- usersom.py
+++ usersom.py
@@ -1,10 +1,28 @@
-first = int(input("enter first :" ))
-second = int(input("enter second :" ))
+# first = int(input("enter first :" ))
+# second = int(input("enter second :" ))
 
-print("sum =", first + second)
+# print("sum =", first + second)
 
 
-side = float(input("enter square :" ))
-print("area =", side * side)
+# side = float(input("enter square :" ))
+# print("area =", side * side)
+
+
+
+
+# a = float(input("enter first :" ))
+# b = float(input("enter second :" ))
+
+# print("avg =", (a+b)/2)
+
+
+a = int(input("enter first :" ))
+b = int(input("enter second :" ))
+
+print(a>=b)
+
+
+
+
 
 
