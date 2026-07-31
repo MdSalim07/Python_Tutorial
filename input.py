527bc6215e59a8bf17784c482d37f9e5e51ecd23
Md Salim <salimm0118@gmail.com>
2026-07-31 11:07:31 +0530

    User input

--- input.py
+++ input.py
@@ -1,2 +1,16 @@
 name = input("enter your name:")
 print("Welcome",name)
+
+
+val = int(input("enter some value :"))
+print(type(val),val)
+
+name = input("enter name :")
+age = int(input("enter age :"))
+marks = float(input("enter marks :"))
+
+print("Welcome",name)
+print("age ;",age )
+print("marks",marks) 
+
+
