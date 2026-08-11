126aed61ca3ee20b23b029fc5a3e1312d4d22417
Md Salim <salimm0118@gmail.com>
2026-08-11 22:42:55 +0530

    practice question

--- stringfunctions.py
+++ stringfunctions.py
@@ -13,5 +13,8 @@
 # str = "I am studying python from Apna college"
 # print(str.find("Apna"))
 
-str = "I am studying Apna python from Apna college"
-print(str.count("Apna"))
+# str = "I am studying python from Apna college"
+# print(str.count("Apna"))
+
+name = input("Enter your name: ")
+print("length of your name is :",len(name))
