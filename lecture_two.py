1ea18e5d40b065eddab4a466a4835374f5241509
Md Salim <salimm0118@gmail.com>
2026-08-11 08:21:13 +0530

    String

--- lecture_two.py
+++ lecture_two.py
@@ -1,16 +1,16 @@
-# str1 = "Hello World"
-# str2 = 'Python is Fun'
-# str3 = """this is a multi line string"""
+str1 = "Hello World"
+str2 = 'Python is Fun'
+str3 = """this is a multi line string"""
 
+print(str1)
+print(str2)
+print(str3)
+
+# str1 = "This is a string .\n we are creating it in python."
 # print(str1)
-# print(str2)
-# print(str3)
 
-str1 = "This is a string .\n we are creating it in python."
-print(str1)
-
-str1 = "This is a string .\t we are creating it in python."
-print(str1)
+# str1 = "This is a string .\t we are creating it in python."
+# print(str1)
 
 # Concatenation of Strings
 
