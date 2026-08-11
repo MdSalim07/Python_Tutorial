f2aeb6bf6f086bd9c341c4c7bb3626e36b915da0
Md Salim <salimm0118@gmail.com>
2026-08-11 08:18:10 +0530

    concatenation of string

--- lecture_two.py
+++ lecture_two.py
@@ -12,25 +12,25 @@
 # str1 = "This is a string .\t we are creating it in python."
 # print(str1)
 
-# Concatenation of Strings
+Concatenation of Strings
 
-# str1 = "Hello"
-# str2 =" World"
-# print(str1 + str2)
+str1 = "Hello"
+str2 =" World"
+print(str1 + str2)
   
 
 
-Length of Strings
-len (str)
+# Length of Strings
+# len (str)
 
-str1 = "Hello"
-len1 = len(str1)
-print(len1)
+# str1 = "Hello"
+# len1 = len(str1)
+# print(len1)
 
-str2 =" World"
-len2 = len(str2)
-print(len2)
-print(str1 + str2)
+# str2 =" World"
+# len2 = len(str2)
+# print(len2)
+# print(str1 + str2)
 
 
 
