43b9dc0305dd3da9763fae2bd0f787f96c0baa33
Md Salim <salimm0118@gmail.com>
2026-08-11 08:16:36 +0530

    length of string

---
+++ lecture_two.py
@@ -0,0 +1,50 @@
+# str1 = "Hello World"
+# str2 = 'Python is Fun'
+# str3 = """this is a multi line string"""
+
+# print(str1)
+# print(str2)
+# print(str3)
+
+# str1 = "This is a string .\n we are creating it in python."
+# print(str1)
+
+# str1 = "This is a string .\t we are creating it in python."
+# print(str1)
+
+# Concatenation of Strings
+
+# str1 = "Hello"
+# str2 =" World"
+# print(str1 + str2)
+  
+
+
+Length of Strings
+len (str)
+
+str1 = "Hello"
+len1 = len(str1)
+print(len1)
+
+str2 =" World"
+len2 = len(str2)
+print(len2)
+print(str1 + str2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- usersom.py
+++ usersom.py
@@ -1,26 +1,21 @@
-# first = int(input("enter first :" ))
-# second = int(input("enter second :" ))
+first = int(input("enter first :" ))
+second = int(input("enter second :" ))
 
-# print("sum =", first + second)
-
-
-# side = float(input("enter square :" ))
-# print("area =", side * side)
+print("sum =", first + second)
 
 
+side = float(input("enter square :" ))
+print("area =", side * side)
+
+a = float(input("enter first :" ))
+b = float(input("enter second :" ))
+
+print("avg =", (a+b)/2)
 
 
-# a = float(input("enter first :" ))
-# b = float(input("enter second :" ))
-
-# print("avg =", (a+b)/2)
-
-
-a = int(input("enter first :" ))
+a = int(input("emter first :"))
 b = int(input("enter second :" ))
-
-print(a>=b)
-
+print (a >= b)
 
 
 
