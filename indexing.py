aa1715c20541b345e379c19b28ad20f77777c1a4
Md Salim <salimm0118@gmail.com>
2026-08-11 21:49:37 +0530

    indexing

--- indexing.py
+++ indexing.py
@@ -1,11 +1,11 @@
-# str = "Apna college"
-# print(str[5])
-
-Slicing of Strings
-
 str = "Apna college"
-print(str[0:5])
-print(str[5:12])
+print(str[5])
+
+# Slicing of Strings
+
+# str = "Apna college"
+# print(str[0:5])
+# print(str[5:12])
 
 # str = "apple"
 # print(str[-5:-1])
