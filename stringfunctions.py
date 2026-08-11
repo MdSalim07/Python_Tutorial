f24abcb3985fc7173f9545b7ec8741736ca081fe
Md Salim <salimm0118@gmail.com>
2026-08-11 22:03:04 +0530

    capitalize

--- stringfunctions.py
+++ stringfunctions.py
@@ -1,10 +1,11 @@
-str = "I am studying python from Apna college"
-print(str.endswith("ege"))
+# str = "I am studying python from Apna college"
+# print(str.endswith("ege"))
 
-str = "I am studying python from Apna college"
-print(str.endswith("eg"))
+# str = "I am studying python from Apna college"
+# print(str.endswith("eg"))
 
-
+str = "i am studying python from Apna college"
+print(str.capitalize())
 
 
 
