3c2a3ab7f51ffb9c958790913fd6480d8937f41a
Md Salim <salimm0118@gmail.com>
2026-08-14 09:21:30 +0530

    condition

--- Conditional.py
+++ Conditional.py
@@ -4,24 +4,24 @@ if(Age >= 18):
     print("con drive")
 
 
-light = "green"
-if(light=="red"):
-    print("stop")
-elif(light =="green"):
-    print("go")
-elif(light== "yellow"):
-    print("look")       
+# light = "green"
+# if(light=="red"):
+#     print("stop")
+# elif(light =="green"):
+#     print("go")
+# elif(light== "yellow"):
+#     print("look")       
 
 
-light = "pink"
-if(light=="red"):
-     print("stop")
-elif(light =="green"):
-    print("go")
-elif(light== "yellow"):
-    print("look") 
-else:
-    print("light is broken")         
+# light = "pink"
+# if(light=="red"):
+#      print("stop")
+# elif(light =="green"):
+#     print("go")
+# elif(light== "yellow"):
+#     print("look") 
+# else:
+#     print("light is broken")         
 
 
 
