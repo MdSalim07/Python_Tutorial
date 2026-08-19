87602bf6ac5a98ec42b353fb72b65af0739db7ce
Md Salim <salimm0118@gmail.com>
2026-08-19 07:57:20 +0530

    count

--- loops.py
+++ loops.py
@@ -3,16 +3,20 @@
 #     print("Hello world!")
 #     count += 1
 
-i = 5
-while i >= 1:
-    print(i)
-    i -= 1
+# i = 5
+# while i >= 1:
+#     print(i)
+#     i -= 1
 
-print ("loops ended")
+# print ("loops ended")
 
+# printNumber from 1 to 100.
 
-
-
+count = 1 
+while count <= 100:
+    print(count)
+    count += 1
+print ("count ended")
 
 
 
