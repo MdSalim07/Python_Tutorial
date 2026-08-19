75803ec08ab2e614700507794e663bd26d1e3e68
Md Salim <salimm0118@gmail.com>
2026-08-19 07:49:49 +0530

    loops

--- loops.py
+++ loops.py
@@ -1,4 +1,31 @@
-count = 1 
-while count <= 5:
-    print("Hello world!")
-    count += 1
+# count = 1 
+# while count <= 5:
+#     print("Hello world!")
+#     count += 1
+
+i = 5
+while i >= 1:
+    print(i)
+    i -= 1
+
+print ("loops ended")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
